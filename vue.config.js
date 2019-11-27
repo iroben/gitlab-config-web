8cad17bd9f870a857bfdc5bf4285cce3075bd4c2
liubin <CodePorter@163.com>
2019-11-27 08:43:20 +0800

    移除vue.config.js一些不必要的语句

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
-const path = require('path');
-
-const resolve = dir => path.join(__dirname, dir);
-
-const urlPath = process.env.NODE_ENV === 'development' ? './' : '/elab-web/';
-
 module.exports = {
     publicPath: './',
     productionSourceMap: false
